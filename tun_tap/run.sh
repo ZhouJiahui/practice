5d7cef1317a11babde69b8741348bf39383fa7ea
zhoujiahui <zjh_coder@qq.com>
2017-03-26 23:42:37 -0400

    mod: implement udp echo

---
+++ tun_tap/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ 
+./tun_interface nt-test0
+ 
--- tun_tap/tun_interface.cc
+++ tun_tap/tun_interface.cc
@@ -9,10 +9,16 @@
 #include <net/if.h>
 #include <sys/ioctl.h>
 #include <linux/if_tun.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/ip.h>
+#include <algorithm>
 
 class TunInterface {
 public:
-  TunInterface(const char *name, bool is_tap, bool no_pi): fd_(open("/dev/net/tun", O_RDWR)) {
+  TunInterface(const char *name, bool is_tap, bool no_pi): fd_(open("/dev/net/tun", O_RDWR)),
+                                                           is_tap_(is_tap),
+                                                           no_pi_(no_pi) {
     if (fd_ < 0) {
       std::cerr << "Error\n";
       std::cerr << strerror(errno) << std::endl;
@@ -26,13 +32,13 @@ public:
      *
      *        IFF_NO_PI - Do not provide packet information
      */
-    if (is_tap) {
+    if (is_tap_) {
       ifr.ifr_flags = IFF_TAP;
     } else {
       ifr.ifr_flags = IFF_TUN;
     }
 
-    if (no_pi) {
+    if (no_pi_) {
       ifr.ifr_flags |= IFF_NO_PI;
     }
 
@@ -56,14 +62,30 @@ public:
         std::cerr << "Error start\n";
         std::cerr << strerror(errno) << std::endl;
       }
-      std::cout << n << std::endl;
-      std::cout << buf << std::endl;
-      memset(buf, 0, sizeof(buf));
+      struct tun_pi * pi = (struct tun_pi *) buf;
+      if (pi->flags & TUN_PKT_STRIP) {
+        std::cerr << "Receive buffer is too small\n"; 
+        continue;
+      }
+      if (pi->proto == htons(ETH_P_IP)) {
+        iphdr *ip_header = reinterpret_cast<iphdr*>(buf + sizeof(tun_pi));
+        std::swap(ip_header->saddr, ip_header->daddr);
+        udphdr *udp_header = reinterpret_cast<udphdr*>(buf + sizeof(tun_pi) + sizeof(iphdr));
+        std::swap(udp_header->source, udp_header->dest);
+        size_t s = sizeof(struct tun_pi) + sizeof(struct iphdr) + sizeof(struct udphdr);
+        const char *data = buf  + s;
+        std::cout << n - s << std::endl;
+        std::cout << data << std::endl;
+        n = write(fd_, buf, n);
+        memset(buf, 0, sizeof(buf));
+      }
     }
   }  
 
 private:
   const int fd_;
+  bool is_tap_;
+  bool no_pi_;
 };
 
 int main(int argc, char ** argv) {
